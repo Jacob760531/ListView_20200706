c5438df92df6264a45d814bea87be1d44d6780e7
조장우 <jopuk2@gmail.com>
2020-07-06 21:39:34 +0900

    실제 데이터 추가

--- app/src/main/java/kr/co/tjoeun/listview_20200706/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listview_20200706/MainActivity.kt
@@ -2,11 +2,21 @@ package kr.co.tjoeun.listview_20200706
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.listview_20200706.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+//    학생목록을 담고 있는 변수
+    val mStudentList  = ArrayList<Student>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(Student("조장우","종로구",1983))
+        mStudentList.add(Student("홍길동","동대문구",1990))
+        mStudentList.add(Student("우뢰매","성북구",1998))
+        mStudentList.add(Student("해리슨포드","강남구",1966))
+
     }
 }
